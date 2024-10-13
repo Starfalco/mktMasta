85c5cad4c60f25e55099b15891ca88b548d84f9d
Nawar Al-Rawi <Nawar@nawars-mbp.home>
2024-10-14 01:27:34 +0200

    1st version of earnings class

--- data_downloader.py
+++ data_downloader.py
@@ -4,29 +4,60 @@ from abc import ABC, abstractmethod
 import requests
 from bs4 import BeautifulSoup
 from getuseragent import UserAgent
+import multitasking
+import signal
+import utils
+import shared
+
+# This is required to handle keyboard interruptions and
+# to kill all threads if such an interruption occurs.
+multitasking.set_max_threads(multitasking.config['CPU_CORES'])
+signal.signal(signal.SIGINT, multitasking.killall)
 
 class Extract(ABC):
-    def __init__(self, ticker, output_path, output_type):
-        self.ticker = ticker
+    def __init__(self, tickers, output_path, output_type):
+        self.tickers = tickers  # expect list of tickers
         self.output_path = output_path
         self.output_type = output_type
-
+    
     @abstractmethod
     def get_data(self):
         pass
 
 class Prices(Extract):
-    def __init__(self, ticker, output_path, output_type, starting_date, ending_date):
-        super().__init__(ticker, output_path, output_type)
+    def __init__(self, tickers, output_path, output_type, starting_date, ending_date):
+        super().__init__(tickers, output_path, output_type)
         self.starting_date = starting_date
         self.ending_date = ending_date
 
     def get_data(self):
-        yf.download(self.ticker, start = self.starting_date, end = self.ending_date, group_by = 'ticker').stack(level=0).to_csv(self.output_path + 'price' + self.output_type, encoding = 'utf-8', sep = ';')
+        yf.download(self.tickers, start = self.starting_date, end = self.ending_date, group_by = 'ticker').stack(level=0).to_csv(self.output_path + 'price' + self.output_type, encoding = 'utf-8', sep = ';')
 
 class Earnings(Extract):
-    def __init__(self, ticker, output_path, output_type):
-        super().__init__(ticker, output_path, output_type)
+    def __init__(self, tickers, output_path, output_type):
+        super().__init__(tickers, output_path, output_type)
+        self.results = []  # list to store all results
 
-    def get_data(self):
-        yf.download(self.ticker, start = self.starting_date, end = self.ending_date, group_by = 'ticker').stack(level=0).to_csv(self.output_path + 'price' + self.output_type, encoding = 'utf-8', sep = ';')
+    @multitasking.task
+    def get_data_for_ticker(self, ticker, progress=True):
+        df = pd.DataFrame(yf.Ticker(ticker).get_earnings_estimate())
+        df['Ticker'] = ticker  # add a column for ticker name
+        self.results.append(df)
+
+        if progress:
+            shared._PROGRESS_BAR.animate()
+
+    def get_data(self, progress=True):
+
+        if progress:
+            shared._PROGRESS_BAR = utils.ProgressBar(len(self.tickers), 'completed')
+
+        for ticker in self.tickers:
+            self.get_data_for_ticker(ticker, progress=(progress))
+        
+        # wait for all tasks to finish before writing the results to a CSV file
+        multitasking.wait_for_tasks()
+
+        # concatenate all dataframes and write to the CSV file
+        all_data = pd.concat(self.results)
+        all_data.to_csv(self.output_path + 'earnings_estimate' + self.output_type, encoding = 'utf-8', sep = ';')
--- main.py
+++ main.py
@@ -4,5 +4,8 @@ file_ticker = './inputs/sp500_list.csv'
 df_ticker = pd.read_csv(file_ticker,encoding='utf-8',sep=';')['Symbol']
 list_ticker = list(df_ticker)
 
-price_extract = Prices(list_ticker,'./extracts/price/','.csv', starting_date='2024-06-01', ending_date='2024-06-30')
-price_extract.get_data()
+# price_extract = Prices(list_ticker,'./extracts/price/','.csv', starting_date='2024-06-01', ending_date='2024-06-30')
+# price_extract.get_data()
+
+earnings_estimate_extract = Earnings(list_ticker,'./extracts/earnings_estimate/','.csv')
+earnings_estimate_extract.get_data()
