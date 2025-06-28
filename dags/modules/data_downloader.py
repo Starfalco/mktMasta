5c03a97ca868fa7049aecd617b0a65f65d27ba61
KwamK-Code <kouassi.kwam@gmail.com>
2025-06-28 19:35:29 +0100

    4 implementing configjson file (#8)
    * Changes to be committed:
    	new file:   dags/modules/config.json
    	modified:   dags/modules/data_downloader.py
    	modified:   dags/modules/extracts_tasks.py
    	new file:   inputs/de/sp500_list.csv
    
    * version 1
    
    * version 1.0
    
    * version 1.1

--- dags/modules/data_downloader.py
+++ dags/modules/data_downloader.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 
 import yfinance as yf
 import pandas as pd
+
 # from deltalake.writer import write_deltalake
 # import requests
 # from bs4 import BeautifulSoup
@@ -12,46 +13,66 @@ import _utils as _utils
 import _shared as _shared
 from requests_ratelimiter import LimiterSession, RequestRate, Limiter, Duration
 
+import json
+
+config_path = config_path = "dags/modules/config.json"
+
+with open(config_path) as stream:
+    config = json.load(stream)
+
 # This is required to handle keyboard interruptions and
 # to kill all threads if such an interruption occurs.
-multitasking.set_max_threads(multitasking.config['CPU_CORES'])
+multitasking.set_max_threads(multitasking.config["CPU_CORES"])
 signal.signal(signal.SIGINT, multitasking.killall)
+
 
 class Extract(ABC):
     def __init__(self, tickers):
         self.tickers = tickers  # expect list of tickers
-        self.request_rate = RequestRate(2, Duration.SECOND)
+        self.request_rate = RequestRate(int(config["request_rate"]), Duration.SECOND)
         self.limiter = Limiter(self.request_rate)
         self.session = LimiterSession(limiter=self.limiter)
-        self.session.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
-        self.folder_path = '/extracts/'
-        # self.folder_path = 'extracts/' # uncomment for ide run
-    
+        self.session.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
+        }
+        self.folder_path = config["extract_path"]
+        # Check config file for IDE path
+
     @abstractmethod
     def get_data(self):
         pass
+
 
 class Prices(Extract):
     def __init__(self, tickers, starting_date, ending_date):
         super().__init__(tickers)
         self.starting_date = starting_date
         self.ending_date = ending_date
-        self.output_path = self.folder_path+ 'price.parquet'
+        self.output_path = self.folder_path + "price.parquet"
 
     def get_data(self):
         # write_deltalake(self.output_path, yf.download(self.tickers, start = self.starting_date, end = self.ending_date, group_by = 'ticker').stack(level=0), mode='overwrite')
-        yf.download(self.tickers, start = self.starting_date, end = self.ending_date, group_by = 'ticker', session=self.session).stack(level=0).to_parquet(self.output_path)
+        yf.download(
+            self.tickers,
+            start=self.starting_date,
+            end=self.ending_date,
+            group_by="ticker",
+            session=self.session,
+        ).stack(level=0).to_parquet(self.output_path)
+
 
 class Earnings_Estimate(Extract):
     def __init__(self, tickers):
         super().__init__(tickers)
         self.results = []  # list to store all results
-        self.output_path = self.folder_path+ 'earnings_estimate.parquet'
+        self.output_path = self.folder_path + "earnings_estimate.parquet"
 
     @multitasking.task
     def get_data_for_ticker(self, ticker, progress=True):
-        df = pd.DataFrame(yf.Ticker(ticker, session=self.session).get_earnings_estimate())
-        df['Ticker'] = ticker  # add a column for ticker name
+        df = pd.DataFrame(
+            yf.Ticker(ticker, session=self.session).get_earnings_estimate()
+        )
+        df["Ticker"] = ticker  # add a column for ticker name
         self.results.append(df)
 
         if progress:
@@ -60,11 +81,11 @@ class Earnings_Estimate(Extract):
     def get_data(self, progress=True):
 
         if progress:
-            _shared._PROGRESS_BAR = _utils.ProgressBar(len(self.tickers), 'completed')
+            _shared._PROGRESS_BAR = _utils.ProgressBar(len(self.tickers), "completed")
 
         for ticker in self.tickers:
             self.get_data_for_ticker(ticker, progress=(progress))
-        
+
         # wait for all tasks to finish before writing the results to a delta_lake file
         multitasking.wait_for_tasks()
 
@@ -72,17 +93,20 @@ class Earnings_Estimate(Extract):
         all_data = pd.concat(self.results)
         # write_deltalake(self.output_path, all_data, mode='overwrite')
         all_data.to_parquet(self.output_path)
+
 
 class Earnings_History(Extract):
     def __init__(self, tickers):
         super().__init__(tickers)
         self.results = []  # list to store all results
-        self.output_path = self.folder_path+ 'earnings_history.parquet'
+        self.output_path = self.folder_path + "earnings_history.parquet"
 
     @multitasking.task
     def get_data_for_ticker(self, ticker, progress=True):
-        df = pd.DataFrame(yf.Ticker(ticker, session=self.session).get_earnings_history())
-        df['Ticker'] = ticker  # add a column for ticker name
+        df = pd.DataFrame(
+            yf.Ticker(ticker, session=self.session).get_earnings_history()
+        )
+        df["Ticker"] = ticker  # add a column for ticker name
         self.results.append(df)
 
         if progress:
@@ -91,11 +115,11 @@ class Earnings_History(Extract):
     def get_data(self, progress=True):
 
         if progress:
-            _shared._PROGRESS_BAR = _utils.ProgressBar(len(self.tickers), 'completed')
+            _shared._PROGRESS_BAR = _utils.ProgressBar(len(self.tickers), "completed")
 
         for ticker in self.tickers:
             self.get_data_for_ticker(ticker, progress=(progress))
-        
+
         # wait for all tasks to finish before writing the results to a delta_lake file
         multitasking.wait_for_tasks()
 
@@ -104,24 +128,27 @@ class Earnings_History(Extract):
         # write_deltalake(self.output_path, all_data, mode='overwrite')
         all_data.to_parquet(self.output_path)
 
+
 class Info(Extract):
     def __init__(self, tickers):
         super().__init__(tickers)
         self.results = []  # list to store all results
-        self.output_path = self.folder_path+ 'info.parquet'
+        self.output_path = self.folder_path + "info.parquet"
 
     @multitasking.task
     def get_data_for_ticker(self, ticker, progress=True):
 
         try:
-            industry = yf.Ticker(ticker, session=self.session).get_info()['industry']
+            industry = yf.Ticker(ticker, session=self.session).get_info()["industry"]
         except:
-            industry = 'unknown'
+            industry = "unknown"
         try:
-            sector = yf.Ticker(ticker, session=self.session).get_info()['sector']
+            sector = yf.Ticker(ticker, session=self.session).get_info()["sector"]
         except:
-            sector = 'unknown'
-        df = pd.DataFrame.from_records({"Ticker": [ticker], "industry": [industry], "sector": [sector]})
+            sector = "unknown"
+        df = pd.DataFrame.from_records(
+            {"Ticker": [ticker], "industry": [industry], "sector": [sector]}
+        )
         self.results.append(df)
 
         if progress:
@@ -130,15 +157,15 @@ class Info(Extract):
     def get_data(self, progress=True):
 
         if progress:
-            _shared._PROGRESS_BAR = _utils.ProgressBar(len(self.tickers), 'completed')
+            _shared._PROGRESS_BAR = _utils.ProgressBar(len(self.tickers), "completed")
 
         for ticker in self.tickers:
             self.get_data_for_ticker(ticker, progress=(progress))
-        
+
         # wait for all tasks to finish before writing the results to a delta_lake file
         multitasking.wait_for_tasks()
 
         # concatenate all dataframes and write to the delta_lake file
         all_data = pd.concat(self.results)
         # write_deltalake(self.output_path, all_data, mode='overwrite')
-        all_data.to_parquet(self.output_path,index=False)
+        all_data.to_parquet(self.output_path, index=False)
--- dags/modules/extracts_tasks.py
+++ dags/modules/extracts_tasks.py
@@ -1,36 +1,51 @@
 import sys
+
 sys.path.append("/opt/airflow/dags/modules/")
 from data_downloader import *
 
+import json
+
+config_path = "dags/modules/config.json"
+
+with open(config_path) as stream:
+    config = json.load(stream)
+
+
 def prices_task():
 
-    price_extract = Prices(get_input(), starting_date='2024-06-01', ending_date='2024-06-30')
+    price_extract = Prices(
+        get_input(), starting_date=config["start_date"], ending_date=config["end_date"]
+    )
     price_extract.get_data()
+
 
 def earnings_estimate_task():
 
     earnings_estimate_extract = Earnings_Estimate(get_input())
     earnings_estimate_extract.get_data()
 
+
 def earnings_history_task():
 
     earnings_history_extract = Earnings_History(get_input())
     earnings_history_extract.get_data()
+
 
 def info_task():
 
     info_extract = Info(get_input())
     info_extract.get_data()
 
+
 def get_input():
-    file_ticker = '/inputs/sp500_list.csv'
-    # file_ticker = 'inputs/sp500_list.csv' # uncomment for ide run
-    df_ticker = pd.read_csv(file_ticker,encoding='utf-8',sep=';')['Symbol']
+    file_ticker = config["path_inputs"]
+    df_ticker = pd.read_csv(file_ticker, encoding="utf-8", sep=";")["Symbol"]
     list_ticker = list(df_ticker)
 
     return list_ticker
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     prices_task()
     # earnings_estimate_task()
