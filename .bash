a6735d7b0382c497b58e71c2d3e8b39c92f0788d
Nawar <92442537+Starfalco@users.noreply.github.com>
2025-07-11 23:34:35 +0200

    Update .bash
    run fastapi.dockerfile not needed

--- .bash
+++ .bash
@@ -4,7 +4,6 @@ docker build . -f airflow.dockerfile --tag extending_airflow:latest
 
 # fastapi container
 docker build . -f fastapi.dockerfile --tag mktmasta_fastapi
-# docker run -d --name mktmasta_api -p 80:80 mktmasta_fastapi
 
 # run all services
 docker compose up -d
