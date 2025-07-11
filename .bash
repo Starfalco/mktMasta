1146619d4108eb3433344b2230be9dcee2cd4e25
Nawar <92442537+Starfalco@users.noreply.github.com>
2025-07-11 23:32:48 +0200

    10 backend fastapi docker compose (#17)
    * fastapi-docker-compose:
    	modified:   docker-compose.yml
    	modified:   fastapi.dockerfile
    
    * deploy fastapi via docker-compose instead
    of dockerflie
    
    * Fast Api Port as 8000
    
    * Changes to be committed:
    	modified:   fastapi.dockerfile
    
    ---------
    
    Co-authored-by: KwamK-Code <kouassi.kwam@gmail.com>

--- .bash
+++ .bash
@@ -1,7 +1,10 @@
 # airflow container
 docker build . -f airflow.dockerfile --tag extending_airflow:latest
-docker compose up -d
+
 
 # fastapi container
 docker build . -f fastapi.dockerfile --tag mktmasta_fastapi
-docker run -d --name mktmasta_api -p 80:80 mktmasta_fastapi
+# docker run -d --name mktmasta_api -p 80:80 mktmasta_fastapi
+
+# run all services
+docker compose up -d
--- fastapi.dockerfile
+++ fastapi.dockerfile
@@ -1,18 +1,3 @@
-
 FROM python:3.12
-
-
-WORKDIR /code
-
-
 COPY ./backend/requirements/base.txt /code/requirements/base.txt
-
-
 RUN pip install --no-cache-dir --upgrade -r /code/requirements/base.txt
-
-
-COPY ./backend/src /code/src
-COPY ./extracts /code/extracts
-
-
-CMD ["fastapi", "run", "src/main.py", "--port", "80"]
